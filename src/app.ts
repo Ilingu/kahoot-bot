0e81d7d19b1aeddedff1f07e05a20903250123b9
Ilingu <thetitouoff@gmail.com>
2022-06-06 17:23:21 +0200

    [WS] Add HTTPS conn to Websocket

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "@fastify/cors";
 import fs from "fs";
 import path from "path";
 import { Server } from "socket.io";
+import { createServer } from "https";
 import { WEB_PORT, WS_PORT } from "./lib/globals";
 // Routes
 import RoutesHandler from "./routes/NewGame";
@@ -10,16 +11,20 @@ import { NewWSConn } from "./lib/WebSockets";
 // Log
 import { log, logError } from "./lib/Utils/utils";
 
+const HTTPS_OPTIONS = {
+  key: fs.readFileSync(
+    path.join(__dirname, "..", "certificates", "privkey.pem")
+  ),
+  cert: fs.readFileSync(
+    path.join(__dirname, "..", "certificates", "fullchain.pem")
+  ),
+};
+
 const server = fastify({
   http2: true,
   https: {
     allowHTTP1: true, // fallback support for HTTP1
-    key: fs.readFileSync(
-      path.join(__dirname, "..", "certificates", "privkey.pem")
-    ),
-    cert: fs.readFileSync(
-      path.join(__dirname, "..", "certificates", "fullchain.pem")
-    ),
+    ...HTTPS_OPTIONS,
   },
 });
 // { requestTimeout: 1800000 } --> 30min/req, don't need it anymore since it run into bg with WS
@@ -27,7 +32,7 @@ const server = fastify({
 // KEY: /etc/letsencrypt/live/kahootbot.xyz/privkey.pem
 
 server.register(cors, {
-  origin: "*",
+  origin: "https://kahootbot-client.vercel.app",
   methods: ["GET", "POST"],
 }); // cors
 server.register(RoutesHandler); // Routes
@@ -38,12 +43,15 @@ server.listen(WEB_PORT, "0.0.0.0", (err, address) => {
     logError(err);
     process.exit(1);
   }
-  log(`Server listening at ${address}`);
+  log(`Web Server listening at ${address}`);
 });
 
 // Websocket server
-const io = new Server(WS_PORT, {
-  cors: { origin: "*" },
+const HttpsServer = createServer(HTTPS_OPTIONS).listen(WS_PORT, () => {
+  log(`WebSocket Server listening at port ${WS_PORT}`);
+});
+const io = new Server(HttpsServer, {
+  cors: { origin: "https://kahootbot-client.vercel.app" },
 });
 
 io.on("connection", NewWSConn);
